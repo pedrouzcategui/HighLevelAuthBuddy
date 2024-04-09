75bc44d2bdb57d50eee9d55115864af68b712948
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-09 19:39:02 -0400

    feature(database): wrap prisma instantiation into singleton

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,4 @@
 import { PrismaClient } from "@prisma/client";
+import { remember } from "@epic-web/remember";
 
-export const db = new PrismaClient();
+export const db = remember("db", () => new PrismaClient());
