ad8bff13f8b234ec8cb7dd089bda330b1bf186fa
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-10 11:59:46 -0400

    feature(api): create api layer using axios

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api",
 });
 
 export const api = {
@@ -15,6 +15,6 @@ export const api = {
 
 export const API_ROUTES = {
   auth: {
-    register: "/auth/register"
-  }
+    register: "/auth/register",
+  },
 };
