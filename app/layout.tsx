53652a12e715378f9dff583f6e8c3988090f89c8
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-10 11:57:15 -0400

    feature(layout): create providers wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Providers } from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(inter.className, "min-h-screen bg-neutral-100")}>
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
