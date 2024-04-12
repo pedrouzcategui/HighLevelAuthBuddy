9f4ec6e4e4cd4c380e2d69f2c29b31d8af16b99f
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-11 23:47:13 -0400

    feature(styles): add destructive and destructive light colors

--- components/common/error-container.tsx
+++ components/common/error-container.tsx
@@ -6,20 +6,19 @@ export type ErrorContainerProps = {
   Icon?: LucideIcon;
 };
 
-// <ShieldOffIcon size={48} className="stroke-red-600" />
 export function ErrorContainer({
   title,
   description,
   Icon,
 }: ErrorContainerProps) {
   return (
-    <section className="flex gap-5 items-center bg-red-200 border-2 border-red-600 rounded-md px-5 py-3">
-      {Icon && <Icon className="h-10 w-10 stroke-red-600" />}
+    <section className="flex gap-5 items-center bg-destructive-light border-2 border-destructive rounded-md px-5 py-3">
+      {Icon && <Icon className="h-10 w-10 stroke-destructive" />}
 
       <div>
-        <header className="text-red-600 font-medium">{title}</header>
+        <header className="text-destructive font-medium">{title}</header>
         {description && (
-          <p className="text-sm text-secondary-foreground">{description}</p>
+          <p className="text-sm text-destructive-foreground">{description}</p>
         )}
       </div>
     </section>
